225da95f0a0eefd78e3aeec67f5f247277252d57
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-22 11:02:25 -0600

    Archivos agregados y estructura del proyecto

---
+++ Academico.Services/EstudianteService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academico.Services
+{
+    public class EstudianteService
+    {
+    }
+}
---
+++ institutoApp.Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academico.Data
+{
+    internal class ApplicationDbContext
+    {
+    }
+}

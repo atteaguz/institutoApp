230dfcf14b7beddcff205e301d09639ffdefa6b7
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-22 14:30:37 -0600

    DTOs de estudiante

--- institutoApp.DTOs/Estudiante/EstudianteCreateDTO.cs
+++ institutoApp.DTOs/Estudiante/EstudianteCreateDTO.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Academico.DTOs.Estudiante
 {
     public class EstudianteCreateDTO
     {
+        [Required(ErrorMessage = "La cedula es obligatoria")]
+        [MaxLength(20, ErrorMessage = "La cédula no puede exceder los 20 caracteres")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El primer apellido es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El primer apellido no puede exceder los 100 caracteres")]
+        public string PrimerApellido { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El segundo apellido no puede exceder los 100 caracteres")]
+        public string? SegundoApellido { get; set; }
+
+        [MaxLength(150, ErrorMessage = "El correo no puede exceder los 150 caracteres")]
+        [EmailAddress(ErrorMessage = "El formato del correo electronico no es vvlido")]
+        public string? CorreoElectronico { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El telefono no puede exceder los 20 caracteres")]
+        [Phone(ErrorMessage = "El formato del telefono no es valido")]
+        public string? Telefono { get; set; }
     }
 }
--- institutoApp.DTOs/Estudiante/EstudianteResponseDTO.cs
+++ institutoApp.DTOs/Estudiante/EstudianteResponseDTO.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Academico.DTOs.Estudiante
+﻿namespace Academico.DTOs.Estudiante
 {
     public class EstudianteResponseDTO
     {
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string? SegundoApellido { get; set; }
+        public string? CorreoElectronico { get; set; }
+        public string? Telefono { get; set; }
+        public DateTime FechaCreacion { get; set; }
     }
 }

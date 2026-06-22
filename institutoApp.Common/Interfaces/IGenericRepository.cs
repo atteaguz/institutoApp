c70fe8528bdfbf09937e597a25d377c78844553c
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-22 14:37:49 -0600

    Interfaces genericas agregadas

--- institutoApp.Common/Interfaces/IGenericRepository.cs
+++ institutoApp.Common/Interfaces/IGenericRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Academico.Common.Interfaces
 {
-    public class IGenericRepository
+    public interface IGenericRepository<E>
     {
+        Task<List<E>> ObtenerTodosAsync();
+        Task<E> ObtenerPorIdAsync(int id);
+        Task<E> CrearAsync(E entity);
+        Task<E> ActualizarAsync(E entity);
+        Task<bool> EliminarAsync(int id);
+        Task<bool> ExistePorNombreAsync(string nombre);
     }
 }
--- institutoApp.Common/Interfaces/IGenericService.cs
+++ institutoApp.Common/Interfaces/IGenericService.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Academico.Common.Response;
 
 namespace Academico.Common.Interfaces
 {
-    public class IGenericService
+    public interface IGenericService<TResponse, TCreate, TUpdate>
     {
+        Task<Response<List<TResponse>>> ObtenerTodosAsync();
+        Task<Response<TResponse>> ObtenerPorIdAsync(int id);
+        Task<Response<TResponse>> CrearAsync(TCreate entity);
+        Task<Response<TResponse>> ActualizarAsync(TUpdate entity);
+        Task<Response<bool>> EliminarAsync(int id);
     }
 }

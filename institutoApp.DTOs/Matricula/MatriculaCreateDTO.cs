d75997e56116dabd131dd47179589016adf34c84
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-22 14:35:13 -0600

    DTOs de matricula

--- institutoApp.DTOs/Matricula/MatriculaCreateDTO.cs
+++ institutoApp.DTOs/Matricula/MatriculaCreateDTO.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Academico.DTOs.Matricula
 {
     public class MatriculaCreateDTO
     {
+        [Required(ErrorMessage = "El ID del estudiante es obligatorio")]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage = "El ID del curso es obligatorio")]
+        public int CursoId { get; set; }
     }
 }
--- institutoApp.DTOs/Matricula/MatriculaResponseDTO.cs
+++ institutoApp.DTOs/Matricula/MatriculaResponseDTO.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Academico.DTOs.Matricula
+﻿namespace Academico.DTOs.Matricula
 {
     public class MatriculaResponseDTO
     {
+        public int Id { get; set; }
+        public int EstudianteId { get; set; }
+        public int CursoId { get; set; }
+        public DateTime FechaMatricula { get; set; }
+
+        //datos para relaciones
+        public string EstudianteNombreCompleto { get; set; }
+        public string EstudianteCedula { get; set; }
+        public string CursoNombre { get; set; }
+        public int CursoCupoMaximo { get; set; }
     }
 }

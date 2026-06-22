0c70b4a8148d660c2282d3e66abb32b3c6ff8ceb
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-22 11:15:20 -0600

    archivos agregados

---
+++ institutoApp.Common/Interfaces/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academico.Common.Interfaces
+{
+    public class IGenericService
+    {
+    }
+}
---
+++ institutoApp.Common/Response/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academico.Common.Response
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool Success { get; set; }
+    }
+}
